77a0a9d2472683bae8882a9edb573efcba0fc786
AlexanderTokar <alextokar@inbox.ru>
2022-09-15 22:33:04 +0300

    Changed test after expert check-up

--- src/test/java/ru/netology/delivery/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/DeliveryTest.java
@@ -10,8 +10,9 @@ import org.openqa.selenium.Keys;
 import ru.netology.delivery.DataGenerator;
 
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class DeliveryTest {
@@ -22,7 +23,7 @@ public class DeliveryTest {
 
     @Test
     @DisplayName("Should successful plan and replan meeting")
-    void shouldSuccessfulPlanAndReplanMeeting() throws InterruptedException {
+    void shouldSuccessfulPlanAndReplanMeeting() {
         Configuration.holdBrowserOpen = true;
         var validUser = DataGenerator.Registration.generateUser("ru");
         var daysToAddForFirstMeeting = 4;
@@ -35,17 +36,14 @@ public class DeliveryTest {
         $("[data-test-id='phone'] input").setValue(validUser.getPhone());
         $("[data-test-id='agreement']").click();
         $x("//*[contains(text(),'Запланировать')]").click();
-        $(".notification__content").shouldBe(visible).shouldHave(text("Встреча успешно запланирована на " + firstMeetingDate));
-        Thread.sleep(1*1000);
+        $(".notification__content").shouldBe(appear).shouldHave(text("Встреча успешно запланирована на " + firstMeetingDate));
         $("[data-test-id=date] input").doubleClick().sendKeys(Keys.DELETE, firstMeetingDate);
         $x("//*[contains(text(),'Запланировать')]").click();
-        $x("//span[contains(text(),'Перепланировать')]").click();
-        $(".notification__content").shouldHave(text("Встреча успешно запланирована на " + firstMeetingDate));
-        Thread.sleep(1*1000);
+        $x("//span[contains(text(),'Перепланировать')]").shouldBe(appear, Duration.ofSeconds(3)).click();
+        $(".notification__content").shouldBe(appear).shouldHave(text("Встреча успешно запланирована на " + firstMeetingDate));
         $("[data-test-id=date] input").doubleClick().sendKeys(Keys.DELETE, secondMeetingDate);
         $x("//*[contains(text(),'Запланировать')]").click();
-        $x("//span[contains(text(),'Перепланировать')]").click();;
-        Thread.sleep(1*1000);
-        $(".notification__content").shouldHave(text("Встреча успешно запланирована на " + secondMeetingDate));
+        $x("//span[contains(text(),'Перепланировать')]").click();
+        $(".notification__content").shouldBe(appear).shouldHave(text("Встреча успешно запланирована на " + secondMeetingDate));
     }
 }
